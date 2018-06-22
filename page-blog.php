fdda7920dca78077cea919c73e6bfe06da8692cb
pinklightsaber <ctorres@idemar.cl>
2018-06-21 22:18:49 -0400

    loop en page-blog, la url a feat_image, quitar comnt etc, y agregar clase de boton rojo

--- page-blog.php
+++ page-blog.php
@@ -28,30 +28,37 @@
 
 						$blog = new WP_Query( $args );
 
-						if ( $blog->have_posts() ) : while ( $blog->have_posts() ) : $blog->the_post();
-						$feat_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
-					?>
+						if ( $blog->have_posts() ) : while ( $blog->have_posts() ) : $blog->the_post(); 
+
+
+					 $feat_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); 
+
+					 ?>
+			
 					<div id="post-<?php the_ID(); ?>" <?php post_class( 'post' ); ?>>
 						<h3>
 							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 								<?php the_title(); ?>
+								
+
 							</a><!--/a-->
 						</h3><!--/h3-->
 						<div class="post-meta">
 							<span>
-								<?php echo get_the_date(); ?> - <?php _e('Posted by:','lawyeria-lite'); ?> <?php the_author_posts_link(); ?> - <?php _e('In category:','lawyeria-lite'); ?> <?php the_category(', '); ?> - <a href="<?php the_permalink(); ?>#comments-template" title="<?php comments_number( __('No responses','lawyeria-lite'), __('One comment','lawyeria-lite'), __('% comments','lawyeria-lite') ); ?>"><?php comments_number( __('No responses','lawyeria-lite'), __('One comment','lawyeria-lite'), __('% comments','lawyeria-lite') ); ?></a>
+								<?php echo get_the_date(); ?> - <?php _e('In category:','lawyeria-lite'); ?> <?php the_category(', '); ?> -
 							</span><!--/span-->
 						</div><!--/div .post-meta-->
 						<?php
 							if ( $feat_image != NULL ) { ?>
 								<div class="post-image">
-									<img src="<?php echo $feat_image[0]; ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" />
+									<a href="<?php the_permalink(); ?>">
+									<img src="<?php echo $feat_image[0]; ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" /></a>
 								</div><!--/.post-image-->
 							<?php } ?>
 						<div class="post-excerpt">
 							<?php the_excerpt(); ?>
 						</div><!--/div .post-excerpt-->
-						<a href="<?php the_permalink(); ?>" title="<?php _e( 'Read More', 'lawyeria-lite' ); ?>" class="read-more">
+						<a href="<?php the_permalink(); ?>" title="<?php _e( 'Read More', 'lawyeria-lite' ); ?>" class=" boton-rojo read-more">
 							<span><?php _e( 'Read More', 'lawyeria-lite' ); ?></span>
 						</a><!--/a .read-more-->
 					</div><!--/div .post-->
--- page.php
+++ page.php
@@ -37,3 +37,14 @@
 			</div><!--/div .wrapper-->
 		</section><!--/section #content-->
 		<?php get_footer(); ?>
+
+		
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+		<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/jquery.event.move.js" type="text/javascript"></script>
+		<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/jquery.twentytwenty.js" type="text/javascript"></script>
+		<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/twentytwenty.css" type="text/css" media="screen" />
+		<script>
+		$(function(){
+		  $("#container1").twentytwenty();
+		});
+		</script>
